7af8f167e2801c00e175c101634055ef649598b8
Leo Mansur <leo.mansur@education.nsw.gov.au>
2021-06-26 10:27:20 +1000

    testing user input

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
-
 using namespace std;
+
+//Variables
+    //States
+bool start = true;
+bool resolve = false;
+bool scan = false;
+bool output = false;
+    //user inputs
+string ip = "";
+string ports = "";
+    // temp storage
+string tmp1 = "";
+string tmp2 = "";
+
 
 int main()
 {
-    cout << "Hello World" << endl;
+    cout << "test";
+    cin >> ip;
+    cout << ip;
 }
