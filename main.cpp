9ce0320fee3f49153e78f69baf789cb9bddd36cd
Leo Mansur <leo.mansur@education.nsw.gov.au>
2021-06-28 20:22:53 +1000

    worked on resolve tried to get exit codes

--- main.cpp
+++ main.cpp
@@ -1,45 +1,64 @@
+//Use g++ for build
 #include <iostream>  //cout
 #include <Windows.h> //Sleep
+#include <stdlib.h> //System
 using namespace std;
 
 //Variables
+    //debug
+bool debug = false;
     //States
+
 bool start = true;
 bool resolve = false;
 bool scan = false;
 bool output = false;
-bool debug = false;
+
 bool uport = false;
     //user inputs
 string host, ports;
     // temp storage
 string tmp1, tmp2;
-char tmp3, tmp4;
+    // commands for terminal
+string ping;
+string port;
 
+void debugexamples(){
+    if (debug == true){
+    //start variables
+    host = "1.1.1.1";
+    ports = "80";
+    //resolve variables
+    //scan variables
+    //output variables
+    cout << "variables have been assigned for skiping" << endl;  
+    }
+    return;
 
+}
 
-int main()
-{
-     while (debug == true){
+int main(){
+   if (debug == true){
+        debugexamples(); //calling debug setting of variables
         cout << "ALERT DEBUG IS ON" << endl;
         cout << "please select subprogram to skip to" << endl;
         cout << "1 for 'start', 2 for 'resolve', 3 for 'scan', 4 for 'output'" << endl;
         cout << "For normal program please select 1" << endl;
-        cin >> tmp3;
-        if (tmp3 == '1'){
+        cin >> tmp1;
+        if (tmp1 == "1"){
             cout << "proceding with normal program" << endl;
         } 
-        else if (tmp3 == '2'){
+        else if (tmp1 == "2"){
             cout << "skiping to resolve" << endl;
             resolve = true;
             start = false;
         }
-        else if (tmp3 == '3'){
+        else if (tmp1 == "3"){
             cout << "skiping to scan" << endl;
             scan = true;
             start = false;
         }
-        else if (tmp3 == '4'){
+        else if (tmp1 == "4"){
             cout << "skiping to output" << endl;
             output = true;
             start = false;
@@ -47,17 +66,13 @@ int main()
         else {
             cout << "invaid responce please select select a vaild responce";
         }
-
-
-        
-
-            }
+    } 
    while (start == true){
        cout << "please input hostname or IP address" << endl;
        cin >> host;
        cout << "would you like to scan ports? (y/N)" << endl;
-       cin >> tmp3;
-       if (tmp3 == 'y') {
+       cin >> tmp2;
+       if (tmp2 == "y") {
            uport = true;
            cout << "Please enter the ports seperated by a comma" << endl;
            cin >> ports;
@@ -67,9 +82,13 @@ int main()
            start = false;
            resolve = true;
        }
-    while (resolve == true){
-        system("cmd" & "ping" && host);
     }
+   
+   if (resolve == true){
+        ping = "ping -a -n 3 " + host;
+        tmp2 = system(ping.c_str());
+        cout << endl << "a" << tmp2;
+        Sleep(5000);
+        cout << tmp2;
    }
-
 }
