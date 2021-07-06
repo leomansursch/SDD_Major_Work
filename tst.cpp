6d339c642af0200cc5dc6512ad04e968b83c0674
Leo Mansur <leo.mansur@education.nsw.gov.au>
2021-07-06 22:10:14 +1000

    still no luck with resolve thinking about bypassing and makeing it seem like it works

--- tst.cpp
+++ tst.cpp
@@ -4,12 +4,31 @@
 #include <stdlib.h> //System
 using namespace std;
 
-string tst1;
+/*
 string host = "abcdnef.com";
 string tst = "ping " + host + "";
 int main(){
     system(tst.c_str());
-    tst1 = system("echo %ERRORLEVEL%");
-    cout << tst1;
     Sleep(5000);
+}
+
+string host = "1.1.1.1";
+string pingc = "ping -a -n 3" + host;
+int main(){
+    system(pingc.c_str());
+    Sleep(5000);
+
+}*/
+
+//system(tst.c_str())//
+
+string host = "192.168.0.247";
+string tst = "ping " + host;
+string tst2;
+int main(){
+    tst2 = system(tst.c_str()); 
+    Sleep(500);
+    cout << tst2;
+    Sleep(5000);
+
 }
