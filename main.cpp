477e6055fdcf4f58f9f95adba12af1d50c377fc7
Leo Mansur <leo.mansur@education.nsw.gov.au>
2021-07-09 19:03:12 +1000

    resolve functioning, got a port scanner working need to find the max array size

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 #include <iostream>  //cout
 #include <Windows.h> //Sleep
 #include <stdlib.h> //System
+#include <winsock2.h>
 using namespace std;
-
+//Each section of the program is broken down into subprograms (Starting(), resolver(), Scaner(), Outputs())
+//This allows me to do further explination about each subprogram while not cluttering up main()
 //Variables
     //debug
 bool debug = false;
@@ -19,83 +21,92 @@ bool uport = false;
 string host, ports;
     // temp storage
 string tmp1, tmp2;
-string tmp3;
+int tmp3;
     // commands for terminal
 string ping;
 string port;
 
 void debugexamples(){
-    if (debug == true){
+    cout << "ALERT DEBUG IS ON" << endl;
+    cout << "variables have been assigned for skiping" << endl;  
     //start variables
     host = "1.1.1.1";
     ports = "80";
     //resolve variables
     //scan variables
     //output variables
-    cout << "variables have been assigned for skiping" << endl;  
+    cout << "please select subprogram to skip to" << endl;
+    cout << "1 for 'start', 2 for 'resolve', 3 for 'scan', 4 for 'output'" << endl;
+    cout << "For normal program please select 1" << endl;
+    cin >> tmp1;
+    if (tmp1 == "1"){
+        cout << "proceding with normal program" << endl;
+    }else if (tmp1 == "2"){
+        cout << "skiping to resolve" << endl;
+        resolve = true;
+        start = false;
+    }else if (tmp1 == "3"){
+        cout << "skiping to scan" << endl;
+        scan = true;
+        start = false;
+    }else if (tmp1 == "4"){
+        cout << "skiping to output" << endl;
+        output = true;
+        start = false;
+    }else {
+        cout << "invaid responce please select select a vaild responce";
     }
     return;
+}
+
+void starting(){ 
+    cout << "please input hostname or IP address" << endl;
+        cin >> host;
+        cout << "would you like to scan ports? (y/N)" << endl;
+        cin >> tmp2;
+        if (tmp2 == "y") {
+            uport = true;
+            cout << "Please enter the ports seperated by a comma" << endl;
+            cin >> ports;
+            start = false;
+            resolve = true;
+        } else if (tmp2 == "n") {
+            start = false;
+            resolve = true;
+        } else {
+            start = false;
+            resolve = true;
+        }
+}
+
+int resolver() { 
+    string tst = "ping " + host + " > NULL 2>&1";
+    int tmp3 = system(tst.c_str());
+    if (tmp3==0){
+        cout<<"success"<<endl;
+        Sleep(5000);
+    }else{
+        cout<<"failed"<<endl;
+        Sleep(5000);
+    }
+    return 0;
+}
+
+int scanner() {
 
 }
 
 int main(){
    if (debug == true){
-        debugexamples(); //calling debug setting of variables
-        cout << "ALERT DEBUG IS ON" << endl;
-        cout << "please select subprogram to skip to" << endl;
-        cout << "1 for 'start', 2 for 'resolve', 3 for 'scan', 4 for 'output'" << endl;
-        cout << "For normal program please select 1" << endl;
-        cin >> tmp1;
-        if (tmp1 == "1"){
-            cout << "proceding with normal program" << endl;
-        } 
-        else if (tmp1 == "2"){
-            cout << "skiping to resolve" << endl;
-            resolve = true;
-            start = false;
-        }
-        else if (tmp1 == "3"){
-            cout << "skiping to scan" << endl;
-            scan = true;
-            start = false;
-        }
-        else if (tmp1 == "4"){
-            cout << "skiping to output" << endl;
-            output = true;
-            start = false;
-        }
-        else {
-            cout << "invaid responce please select select a vaild responce";
-        }
+        debugexamples(); //calling debug setting of variable
     } 
-   while (start == true){
-       cout << "please input hostname or IP address" << endl;
-       cin >> host;
-       cout << "would you like to scan ports? (y/N)" << endl;
-       cin >> tmp2;
-       if (tmp2 == "y") {
-           uport = true;
-           cout << "Please enter the ports seperated by a comma" << endl;
-           cin >> ports;
-           start = false;
-           resolve = true;
-       } else {
-           start = false;
-           resolve = true;
-       }
-    }
-   
+   if (start == true){
+        starting();
+    }   
    if (resolve == true){
-        /*ping = "ping -a -n 3 " + host + " > nul";
-        tmp2 = system(ping.c_str());
-        cout << endl << "a" << tmp2;
-        Sleep(5000);
-        cout << tmp2;*/
-        pingr();
-   }
+        resolver();
+    }
+   if (scan == true){
+       scanner();
+    }
 }
-
-int pingr() { //named pingr due to running out of var ideas.
-    string pingc = "ping -a -n 3" + host;
-    if (system(pingc.c_str())) puts ("0");
-        cout << "yay";
--- tst.cpp
+++ tst.cpp
@@ -1,9 +1,13 @@
-//Use g++ for build
-#include <iostream>  //cout
+/*//Use g++ for build
+
+#include <winsock2.h>
 #include <Windows.h> //Sleep
 #include <stdlib.h> //System
-using namespace std;
 
+
+using namespace std;*/
+#include <iostream>  //cout
+using namespace std;
 /*
 string host = "abcdnef.com";
 string tst = "ping " + host + "";
@@ -21,7 +25,7 @@ int main(){
 }*/
 
 //system(tst.c_str())//
-
+/*
 string host = "192.168.0.247";
 string tst = "ping " + host;
 string tst2;
@@ -31,4 +35,104 @@ int main(){
     cout << tst2;
     Sleep(5000);
 
+}*/
+/*
+int main() {
+string host;
+cout << "put in host" << endl;
+cin >> host;
+string tst = "ping " + host + " > NULL 2>&1";
+int x = system(tst.c_str());
+if (x==0){
+    cout<<"success"<<endl;
+    Sleep(5000);
+}else{
+    cout<<"failed"<<endl;
+    Sleep(5000);
+}
+}*/
+
+
+//now testing sending TCP packets for Scanner()
+
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+
+// Need to link with Ws2_32.libB
+#pragma comment(lib, "ws2_32.lib")
+string host = "192.168.0.247";
+int port = {1,2,3,54,89,66};
+int tmp1 = size(port);
+int main()
+for (int i = 0; i < tmp1; i++) {
+    cout << port[i] << endl;
+}
+
+
+/*
+{
+    //----------------------
+    // Initialize Winsock
+    WSADATA wsaData;
+    int iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != NO_ERROR) {
+        wprintf(L"WSAStartup function failed with error: %d\n", iResult);
+        return 1;
+    }else{
+        cout << "tst";
+    }
+    //----------------------
+    // Create a SOCKET for connecting to server
+    SOCKET ConnectSocket;
+    ConnectSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (ConnectSocket == INVALID_SOCKET) {
+        wprintf(L"socket function failed with error: %ld\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }else{
+        cout << "socket create";
+    }
+    //----------------------s
+    // The sockaddr_in structure specifies the address family,
+    // IP address, and port of the server to be connected to.
+    sockaddr_in clientService;
+    clientService.sin_family = AF_INET;
+    clientService.sin_addr.s_addr = inet_addr(host.c_str());
+    clientService.sin_port = htons(port);
+    cout << clientService.sin_family << endl <<  clientService.sin_addr.s_addr << endl << clientService.sin_port;
+
+    //----------------------
+    // Connect to server.
+    iResult = connect(ConnectSocket, (SOCKADDR *) & clientService, sizeof (clientService));
+    if (iResult == SOCKET_ERROR) {
+        wprintf(L"connect function failed with error: %ld\n", WSAGetLastError());
+        iResult = closesocket(ConnectSocket);
+        if (iResult == SOCKET_ERROR)
+            wprintf(L"closesocket function failed with error: %ld\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+    else{
+        cout << "wtf is going on";
+    }
+
+    wprintf(L"Connected to server.\n");
+
+    iResult = closesocket(ConnectSocket);
+    if (iResult == SOCKET_ERROR) {
+        wprintf(L"closesocket function failed with error: %ld\n", WSAGetLastError());
+        WSACleanup();
+        return 1;
+    }
+
+    WSACleanup();
+    return 0;
+    */
 }
